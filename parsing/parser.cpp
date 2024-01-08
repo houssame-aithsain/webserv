e8723aa6dfa7961f597322f90c4613debed57072
Guennach Othmane <gothmane@c2r5p9.1337.ma>
2024-01-08 13:22:47 +0100

    Spliting The server and location and making them linked in struct

--- parsing/parser.cpp
+++ parsing/parser.cpp
@@ -6,36 +6,25 @@
 /*   By: gothmane <gothmane@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 16:08:13 by gothmane          #+#    #+#             */
-/*   Updated: 2024/01/07 17:42:08 by gothmane         ###   ########.fr       */
+/*   Updated: 2024/01/08 13:10:40 by gothmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.hpp"
 
-Parser::Parser(){}
+Parser::Parser() {}
 
-Parser::~Parser(){}
+Parser::~Parser() {}
 
-Parser::Parser(const Parser& p) 
+Parser::Parser(const Parser &p)
 {
-    this->server = p.server;
 }
 
-Parser& Parser::operator=(const Parser& pr)
+Parser &Parser::operator=(const Parser &pr)
 {
-    if (this != &pr)
-        this->server = pr.server;
+    // if (this != &pr)
+    //     this->server = pr.server;
     return (*this);
-}
-
-
-std::vector<std::pair<std::string, std::vector<std::string> > > Parser::getServer() {
-    return this->server;
-}
-
-void Parser::setServer(const std::vector<std::pair<std::string, std::vector<std::string> > > &server) 
-{
-    this->server = server;
 }
 
 
@@ -46,29 +35,94 @@ void Parser::ft_read_nd_parse(std::string fileName)
     std::string line;
     int server_side = 0;
     int location_side = 0;
-    
+    int server_id = 0;
+    int i = 0;
+    int j = -1;
+
     if (in.is_open())
     {
         while (std::getline(in, line))
         {
             if (line == "[[server]]")
-                server_side = 1;
+            {
+                if (server_side > 0)
+                {
+                    wrapper.push_back(data);
+                    data.server.clear();
+                    data.location.clear();                
+                }
+                server_side++;
+                location_side = 0;
+                j = -1;
+                continue;
+            }
             else if (line == "	[[server.location]]")
             {
-                server_side = 0;
-                location_side = 1;
+                // server_side = 0;
+                server_id = server_side;
+                location_side++;
+                j++;
+                std::cout << "Server id = " << server_id << " location id = " << location_side << std::endl;
+                continue;
             }
-            if (server_side == 1)
+            if (server_id != server_side && location_side == 0)
             {
                 std::string key = line.substr(0, line.find("="));
-                std::cout << key << std::endl;
+                std::vector<std::string> value;
+                value.push_back(line.substr(line.find("=") + 1, line.size()));
+                data.server.push_back(std::make_pair(key, value));
+                // server.erase();
             }
-            else if (location_side == 1)
+            else if (location_side > 0)
             {
+                // std::cout << "In location side ====> ";
                 std::string key = line.substr(0, line.find("="));
-                std::cout << key << std::endl;
+                std::vector<std::string> value;
+                value.push_back(line.substr(line.find("=") + 1, line.size()));
+                std::cout << j << std::endl;
+                if (j >= data.location.size()) {
+                    data.location.resize(j+1);
+                }
+                data.location.at(j).push_back(std::make_pair(key, value));
+                // std::cout << key << std::endl;
+                // std::cout << std::endl;
             }
+        }
+        if (server_side > 0)
+            wrapper.push_back(data);
+    }
+std::size_t r = 1;
+for (std::vector<server_data>::iterator it = wrapper.begin(); it != wrapper.end(); ++it, ++r) {
+    std::cout << "Server data " << i << ":\n";
+    
+    std::cout << "#Server:\n";
+    std::size_t j = 1;
+    for (std::vector<std::pair<std::string, std::vector<std::string> > >::iterator jt = it->server.begin(); jt != it->server.end(); ++jt, ++j) {
+        std::string key = jt->first;
+        std::vector<std::string> values = jt->second;
+
+        std::cout << "Key: " << key << ", Values: ";
+        for (std::size_t k = 0; k < values.size(); ++k) {
+            std::cout << values[k] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "#Location:\n";
+    std::size_t k = 1;
+    for (std::vector<std::vector<std::pair<std::string, std::vector<std::string> > > >::iterator kt = it->location.begin(); kt != it->location.end(); ++kt, ++k) {
+        std::cout << "Location Number : " << k << std::endl;
+        std::size_t l = 1;
+        for (std::vector<std::pair<std::string, std::vector<std::string> > >::iterator lt = kt->begin(); lt != kt->end(); ++lt, ++l) {
+            std::string key = lt->first;
+            std::vector<std::string> values = lt->second;
+
+            std::cout << "Key: " << key << ", Values: ";
+            for (std::size_t m = 0; m < values.size(); ++m) {
+                std::cout << values[m] << " ";
+            }
+            std::cout << std::endl;
         }
     }
 }
-
+}
--- parsing/parser.hpp
+++ parsing/parser.hpp
@@ -6,7 +6,7 @@
 /*   By: gothmane <gothmane@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 16:08:15 by gothmane          #+#    #+#             */
-/*   Updated: 2024/01/07 17:39:02 by gothmane         ###   ########.fr       */
+/*   Updated: 2024/01/08 12:57:00 by gothmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,24 @@
 #include <vector>
 #include <map>
 
+typedef struct t_server_data
+{
+    std::vector<std::pair<std::string, std::vector<std::string> > > server;
+    std::vector<std::vector<std::pair<std::string, std::vector<std::string> > > > location;
+}           server_data;
+
 class Parser
 {
     private:
-        std::vector<std::vector <std::pair<std::string, std::vector<std::string> > > > wrapper;
-        std::vector<std::pair<std::string, std::vector<std::string> > > server;
-        std::vector<std::pair<std::string, std::vector<std::string> > > location;
+        std::vector <server_data> wrapper;
+        server_data data;
    public:
         Parser();
         Parser(const Parser& p);
         Parser& operator=(const Parser& p);
         ~Parser();
-        std::vector<std::pair<std::string, std::vector<std::string> > > getServer();
-        void setServer(const std::vector<std::pair<std::string, std::vector<std::string> > > &server);
+        // std::vector<std::pair<std::string, std::vector<std::string> > > getServer();
+        // void setServer(const std::vector<std::pair<std::string, std::vector<std::string> > > &server);
 
         void ft_read_nd_parse(std::string fileName);
 
